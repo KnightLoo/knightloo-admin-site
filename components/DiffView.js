a037d9f76182b37a5ba4bfe8c97c62b0379f2d58
Todd Weidler <toddweidler@knights.ucf.edu>
2021-11-30 12:27:24 -0500

    added diff page

---
+++ components/DiffView.js
@@ -0,0 +1,235 @@
+import React from 'react';
+import {db} from '../firebaseApp';
+import {getDoc, getDocs, query, where, collection, onSnapshot, writeBatch, doc, updateDoc} from "firebase/firestore";
+import { useState, useEffect } from 'react';
+
+function findHopDiffs(originalHopData, reportedIssueHopData){
+
+    let hopDiffs = [null, null, null, null, null, null, null];
+
+    for(let i=0; i<7; i++){
+
+        const oHop = originalHopData[i];
+        const eHop = reportedIssueHopData[i];
+
+
+
+        if(oHop != null){
+
+            if(eHop != null){
+
+                if(eHop.etRangeStr == oHop.etRangeStr){
+                    hopDiffs[i] = {"orig": oHop.etRangeStr, "new": "N/A"};
+                }
+                else {
+                    hopDiffs[i] = {"orig": oHop.etRangeStr, "new": eHop.etRangeStr};
+                }
+            } else {
+
+                if(oHop.etRangeStr == "closed"){
+                    hopDiffs[i] = {"orig": "Closed", "new": "N/A"};
+                } else {
+                    hopDiffs[i] = {"orig": oHop.etRangeStr, "new": "Closed"};
+                }
+            }
+        } else {
+
+            if(eHop != null){
+                hopDiffs[i] = {"orig": "Closed", "new": eHop.etRangeStr};
+            } else {
+                hopDiffs[i] = {"orig": "Closed", "new": "N/A"};
+            }
+        }
+    }
+
+    return hopDiffs;
+
+}
+
+export default function DiffView({setShowingDiffView, diffLandmark}) {
+
+    const [fullLandmarkData, setFullLandmarkData] = useState(null);
+
+    const handleBack = () => {
+        setShowingDiffView(false);
+    }
+
+
+    useEffect(() => {
+        
+        async function getBathroomData(){
+
+            if(diffLandmark != null){
+                try {
+
+                    const bathroomDocRef = doc(db, "landmark_locations_data", diffLandmark.landmark_id);
+    
+                    const docSnap = await getDoc(bathroomDocRef);
+    
+                    if (docSnap.exists()) {
+    
+                        console.log("full doc data retrieved");
+                        
+                        const fullData = {...diffLandmark, ...docSnap.data()};
+                        const hopDiffs = findHopDiffs(fullData.hopData.flattenedHopDataForFilteringAndMutating, fullData.edited_hop_data.flattenedHopDataForFilteringAndMutating);
+
+                        setFullLandmarkData({...fullData, "hopDiffs": hopDiffs});
+    
+                    } else {
+                        console.log("Bathroom document not found!!!");
+                    }
+    
+                } catch(error){
+                    console.log("Error getting bathroom data: ", error);
+                }
+
+            } else {
+                console.log("diffLandmark is null");
+            }
+
+        }
+
+        getBathroomData();
+        
+
+    }, [diffLandmark]);
+
+    // return (
+    //     <div>
+    //         <button className="btn btn-link" onClick={handleBack}>Back</button>
+
+    //         <div className="card container">
+    //             <div className="row">
+    //                 <div className="col-sm">
+    //                     <div>
+    //                         {/* Bathroom Building Name Diff: */}
+    //                     </div>
+                        
+    //                 </div>
+    //                 <div className="col-sm">
+    //                     <div>
+
+    //                     </div>
+    //                 One of three columns
+    //                 </div>
+    //                 <div className="col-sm">
+    //                 One of three columns
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     </div>
+    // );
+
+    const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+    return (
+        <>
+            {fullLandmarkData != null ?
+            <div>
+                <button className="btn btn-link" onClick={handleBack}>Back</button>
+
+                <div className="container">
+                    <div className="row">
+
+                        <div className="col">
+                            <div className="px-3 mb-3 mt-3">
+                                <h5>Building Name Diff</h5>
+                            </div>
+
+                            <div className="container pt-0">
+                                <div className="row">
+                                    <div className="col-12">
+                                        <table className="table table-bordered">
+                                            <thead>
+                                                <tr>
+                                                    <th scope="col">Original</th>
+                                                    <th scope="col">Proposed Update</th>
+                                                </tr>
+                                                </thead>
+                                            <tbody>
+                                                    <tr>
+                                                        <td>{fullLandmarkData.building}</td>
+                                                        <td>{fullLandmarkData.edited_bathroom_name}</td>
+                                                    </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        
+
+                        {/* <div className="col"> */}
+                            <div className="px-3 mb-3">
+                                <h5>Location Diff</h5>
+                            </div>
+                            
+                            <div className="container pt-0">
+                                <div className="row">
+                                    <div className="col-12">
+                                        <table className="table table-bordered">
+                                            <thead>
+                                                <tr>
+                                                    <th scope="col"></th>
+                                                    <th scope="col">Original</th>
+                                                    <th scope="col">Proposed Update</th>
+                                                </tr>
+                                                </thead>
+                                            <tbody>
+                                                    <tr>
+                                                        <th className="">Longitude</th>
+                                                        <td>{fullLandmarkData.longitude}</td>
+                                                        <td>{fullLandmarkData.edited_location_data ? fullLandmarkData.edited_location_data.longitude : "N/A" }</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th className="">Latitude</th>
+                                                        <td>{fullLandmarkData.latitude}</td>
+                                                        <td>{fullLandmarkData.edited_location_data ? fullLandmarkData.edited_location_data.latitude : "N/A" }</td>
+                                                    </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col">
+                            <div className="px-3 mb-3">
+                                <h5>Hours of Operation Diff</h5>
+                            </div>
+                            
+                            <div className="container pt-0">
+                                <div className="row">
+                                    <div className="col-12">
+                                        <table className="table table-bordered">
+                                            <thead>
+                                                <tr>
+                                                    <th scope="col"></th>
+                                                    <th scope="col">Original</th>
+                                                    <th scope="col">Proposed Update</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                {fullLandmarkData.hopDiffs && fullLandmarkData.hopDiffs.map((hopDiff, index) => (
+                                                    <tr key={index}>
+                                                        <th className="">{daysOfWeek[index]}</th>
+                                                        <td>{hopDiff.orig}</td>
+                                                        <td>{hopDiff.new}</td>
+                                                        {/* <td>{fullLandmarkData.hopData.flattenedHopDataForFilteringAndMutating[dayInd] != null ? fullLandmarkData.hopData.flattenedHopDataForFilteringAndMutating[dayInd].etRangeStr : "Closed" }</td>
+                                                        <td>{fullLandmarkData.edited_hop_data.flattenedHopDataForFilteringAndMutating[dayInd] != null ? fullLandmarkData.edited_hop_data.flattenedHopDataForFilteringAndMutating[dayInd].etRangeStr : "Closed"}</td> */}
+                                                    </tr>
+                                                ))}
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> : 
+                
+            <div>Loading...</div>
+            }
+        </>
+        
+    )
+}
--- components/ReportedIssuesView.js
+++ components/ReportedIssuesView.js
@@ -2,11 +2,64 @@ import React from 'react';
 import {db} from '../firebaseApp';
 import {getDocs, query, where, collection, onSnapshot, writeBatch, doc, updateDoc} from "firebase/firestore";
 import { useState, useEffect } from 'react';
+import DiffView from './DiffView';
+import cloneDeep from 'lodash/cloneDeep';
 
+
+function findHopDiffs(originalHopData, reportedIssueHopData){
+
+    let hopDiffs = [null, null, null, null, null, null, null];
+
+    let areThereDiffs = false;
+
+    for(let i=0; i<7; i++){
+
+        const oHop = originalHopData[i];
+        const eHop = reportedIssueHopData[i];
+
+
+
+        if(oHop != null){
+
+            if(eHop != null){
+
+                if(eHop.etRangeStr == oHop.etRangeStr){
+                    hopDiffs[i] = {"orig": oHop.etRangeStr, "new": "N/A"};
+                }
+                else {
+                    hopDiffs[i] = {"orig": oHop.etRangeStr, "new": eHop.etRangeStr};
+                    areThereDiffs = true;
+                }
+            } else {
+
+                if(oHop.etRangeStr == "closed"){
+                    hopDiffs[i] = {"orig": "Closed", "new": "N/A"};
+                } else {
+                    hopDiffs[i] = {"orig": oHop.etRangeStr, "new": "Closed"};
+                    areThereDiffs = true;
+                }
+            }
+        } else {
+
+            if(eHop != null){
+                hopDiffs[i] = {"orig": "Closed", "new": eHop.etRangeStr};
+                areThereDiffs = true;
+            } else {
+                hopDiffs[i] = {"orig": "Closed", "new": "N/A"};
+            }
+        }
+    }
+
+    return {areThereDiffs: areThereDiffs, hopDiffs: hopDiffs};
+
+}
 
 export default function ReportedIssuesView() {
 
     const [reportedIssues, setReportedIssues] = useState(null);
+    const [fullIssueData, setFullIssueData] = useState(null);
+    const [showingDiffView, setShowingDiffView] = useState(false);
+    const [diffLandmark, setDiffLandmark] = useState(null); 
 
     useEffect(() => {
 
@@ -49,7 +102,71 @@ export default function ReportedIssuesView() {
 
         return unsubscribe;
         
-    }, [])
+    }, []);
+
+
+
+    useEffect(() => {
+        
+        async function getLandmarkDocs(){
+
+            if(reportedIssues != null && reportedIssues.length > 0){
+
+                try {
+                    const landmarkIds = reportedIssues.map(issue => issue.landmark_id);
+
+
+                    if(landmarkIds == null || landmarkIds.length < 1){
+                        console.log("Unexpected error. Landmark ids is null or empty");
+                        return;
+                    }
+
+                    const landmarksRef = collection(db, "landmark_locations_data");
+            
+                    const q = query(landmarksRef, where("id", "in", landmarkIds));
+            
+                    const querySnapshot = await getDocs(q);
+    
+                    let fullIssues = cloneDeep(reportedIssues);
+
+                    let landmarkMap = new Map();
+
+                    querySnapshot.forEach((doc) => {
+                            landmarkMap.set(doc.id, doc.data());
+                        }
+                    );
+
+                    fullIssues = fullIssues.map(partialIssue => {
+                        return {
+                            ...landmarkMap.get(partialIssue.landmark_id),
+                            ...partialIssue
+                        };
+                    });
+
+                    fullIssues = fullIssues.map(issue => {
+
+                        const {areThereDiffs, hopDiffs} = findHopDiffs(issue.hopData.flattenedHopDataForFilteringAndMutating, issue.edited_hop_data.flattenedHopDataForFilteringAndMutating)
+
+                        return {
+                            ...issue,
+                            hopDiffs: hopDiffs,
+                            wasHoursEdited: areThereDiffs
+                        };
+                    });
+
+                    console.log("setting full issue data");
+                    setFullIssueData(fullIssues);
+                    
+                } catch(error){
+                    console.log(error);
+                }
+            }
+        }
+
+        getLandmarkDocs();
+        
+        
+    }, [reportedIssues]);
 
     const handleAccept = async (reportedIssue) => {
 
@@ -113,8 +230,9 @@ export default function ReportedIssuesView() {
         }
     }
 
-    const showDiff = (id) => {
-        console.log("showing diff of: ", id);
+    const showDiff = (reportedIssue) => {
+        setDiffLandmark(reportedIssue);
+        setShowingDiffView(true);
     }
 
     const dummyData = [
@@ -136,8 +254,11 @@ export default function ReportedIssuesView() {
 
 
     return (
-        <div>
-            <div className="container">
+
+        <>
+        {!showingDiffView ? 
+            <div>
+                <div className="container">
                 <div className="row">
                     <div className="col-12">
                         <table className="table table-bordered">
@@ -152,7 +273,7 @@ export default function ReportedIssuesView() {
                             </thead>
                             <tbody>
 
-                                {reportedIssues && reportedIssues.length > 0 ? reportedIssues.map(reportedIssue => (
+                                {fullIssueData && fullIssueData.length > 0 ? fullIssueData.map(reportedIssue => (
                                     <tr key={reportedIssue.landmark_id}>
                                         {/* <th scope="row">1</th> */}
                                         <td>{reportedIssue.bathroomName}</td>
@@ -162,7 +283,7 @@ export default function ReportedIssuesView() {
                                         <td>
                                             <button type="button" className="btn btn-link action-btn-accept" onClick={() => handleAccept(reportedIssue)}><i className="bi bi-check-lg"></i></button>
                                             <button type="button" className="btn btn-link action-btn-reject" onClick={() => handleReject(reportedIssue)}><i className="bi bi-x-lg"></i></button>
-                                            <button type="button" className="btn btn-link action-btn-diff" onClick={() => showDiff(reportedIssue.landmark_id)}><i className="bi bi-file-diff"></i></button>
+                                            <button type="button" className="btn btn-link action-btn-diff" onClick={() => showDiff(reportedIssue)}><i className="bi bi-file-diff"></i></button>
                                         </td>
                                     </tr>
                                 )) : 
@@ -175,6 +296,9 @@ export default function ReportedIssuesView() {
                     </div>
                 </div>
                 </div>
-        </div>
+            </div> : <DiffView setShowingDiffView={setShowingDiffView} diffLandmark={diffLandmark} />
+        }
+        </>
+        
     )
 }
